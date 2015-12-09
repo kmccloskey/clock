920fb64c62aace5e5b47f2c6620b21338d8e7a15
Kyle McCloskey <mccloskeykyle@gmail.com>
2015-12-09 17:06:27 -0500

    working clock, not ideal code

--- clock.js
+++ clock.js
@@ -1,10 +1,34 @@
 $(document).ready( function() {
+
 	setInterval(function(){
 		var date = new Date();
-		$(".hour").text(date.getHours());
-		$(".minute").text(date.getMinutes());
-		$(".second").text(date.getSeconds());
-		$(".millisecond").text(date.getMilliseconds());
+		var hour = date.getHours();
+		var minute = date.getMinutes()
+		var second = date.getSeconds();
+		var millisecond = date.getMilliseconds();
+
+		if(hour > 12) {
+		// console.log(hour - 12)
+		hour -= 12;
+		};
+
+		if(hour == 0) {
+			hour == 12;
+		};
+
+		if (hour < 10) {
+			hour = ('0' + hour);
+		}
+
+		if (minute < 10) {
+			minute = ('0' + minute)
+		}
+
+		$(".hour").text(hour);
+		$(".minute").text(minute);
+		$(".second").text(second);
+		$(".millisecond").text(millisecond);
 	}, 1000);
+
 
 });
